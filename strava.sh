caf4247ed3b7204b9c0750c25667314e4737a72b
Dominik Bułaj <dominik@bulaj.com>
2019-11-27 16:16:04 +0100

    Change last_id to next_id

--- strava.sh
+++ strava.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-START_ID=$(cat last_id)
+START_ID=$(cat next_id)
 END_ID=$((START_ID + 20))
 echo "Checking challenges from ID ${START_ID} to ${END_ID}"
 
@@ -9,6 +9,6 @@ for i in $(seq "$START_ID" $END_ID); do
   TITLE=$(curl -s "$CURL_URL" | awk -F "<" '/<title>/ {print $2}' | awk -F ">" '{print $2}')
   if [ "$TITLE" ]; then
     echo "${CURL_URL} - ${TITLE}"
-    echo $((i + 1)) >last_id #save last available challenge id to file
+    echo $((i + 1)) >next_id #save last available challenge id to file
   fi
 done
