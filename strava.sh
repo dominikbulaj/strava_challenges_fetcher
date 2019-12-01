05c64bd85c03f0191a1b16388b9f5edc26002ee9
Dominik Bułaj <dominik@bulaj.com>
2019-12-01 22:24:04 +0100

    One AWK command due to using regular expression for input field separator

--- strava.sh
+++ strava.sh
@@ -6,7 +6,7 @@ echo "Checking challenges from ID ${START_ID} to ${END_ID}"
 
 for i in $(seq "$START_ID" $END_ID); do
   CURL_URL="https://www.strava.com/challenges/${i}"
-  TITLE=$(curl -s "$CURL_URL" | awk -F "<" '/<title>/ {print $2}' | awk -F ">" '{print $2}')
+  TITLE=$(curl -s "$CURL_URL" | awk -F "[<>]" '/<title>/ {print $3}')
   if [ "$TITLE" ]; then
     echo "${CURL_URL} - ${TITLE}"
     echo $((i + 1)) >next_id #save last available challenge id to file
